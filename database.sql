aa3b153a031d2bc59bc1a9247327091b7fe289f4
Steve Frend <stevefrend1@gmail.com>
2020-04-18 16:03:00 -0700

    finished basic routes for database and tested - working

--- database.sql
+++ database.sql
@@ -13,3 +13,10 @@ VALUES ('nameOfAlgo', 'password');
 
 -- get a result that matches user_id/algo_id
 SELECT * FROM times WHERE user_id = 1 AND algo_id = 1;
+
+
+-- store a result in time table, connecting with user_id and algo_id
+INSERT INTO times (time, algo_id, user_id) VALUES ($1, $2, $3);
+
+-- add user to database
+INSERT INTO users (name, password) VALUES($1, $2);
--- server/route/algoroutes.js
+++ server/route/algoroutes.js
@@ -4,19 +4,36 @@ const pool = require('../../db')
 const router = express.Router();
 
 // Get a algo for a specific ID number (random num is generated in front end)
-router.get('/algo/:id', /**middleware */ async (req,res)=>{
+router.get('/algo/:id', async (req,res, next)=>{
   const query = 'SELECT * FROM algos WHERE algo_id = $1;'
   try {
     const algo = await pool.query(query, [req.params.id])
     res.status(200).json(algo.rows[0])
-  } catch (error) {
-    return next(error)
+  } catch (err) {
+    return next(err)
   }
-})
+});
 
-router.post('/storeResult', (req, res ) => {
-  
-  res.status(200).json('stored');
-})
+// Receive a body that contains TIME, ALGO_ID and USER_ID for storing in 'times' table
+router.post('/storeResult', async (req, res, next ) => {
+  const { user_id, algo_id, timeInSeconds } = req.body;
+  const query = `INSERT INTO times (time, algo_id, user_id) VALUES ($1, $2, $3);`
+  try {
+    const algo = await pool.query(query, [timeInSeconds, algo_id, user_id]);
+    res.status(200).json(`Successfully added result to 'time' table`)
+  } catch (err) {
+    return next(err)
+  }
+});
+
+router.post('/addUser', async (req, res, next) => {
+  const query = `INSERT INTO users (name, password) VALUES($1, $2);`
+  try {
+    await pool.query(query, [req.body.username, req.body.password]);
+    res.status(200).json(`Successfully added ${req.body.username} to the users table`)
+  } catch (err) {
+    return next(err)
+  }
+});
 
 module.exports = router;
