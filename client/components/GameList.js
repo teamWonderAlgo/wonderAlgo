19a7cf1227b7d5498feac2197435d4902187310c
Steve Frend <stevefrend1@gmail.com>
2020-04-18 10:04:31 -0700

    pre deleting previous files

--- client/components/GameList.js
+++ client/components/GameList.js
@@ -3,15 +3,15 @@ import PropTypes from 'prop-types';
 
 const GameList = (props) => {
   const gameList = props.gameList;
-  const listElements = gameList.map(game => (
-    <li key={game.createdAt}>{game.winner} won at {game.createdAt}</li>
+  const listElements = gameList.map((game) => (
+    <li key={game.createdAt}>
+      {game.winner} won at {game.createdAt}
+    </li>
   ));
   return (
-    <div id="gameList">
-      <h3>Previous matches</h3>
-      <ul>
-        {listElements}
-      </ul>
+    <div id='gameList'>
+      <h3>Past matches</h3>
+      <ul>{listElements}</ul>
     </div>
   );
 };
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    mode: process.env.NODE_ENV,
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/build/',
+    contentBase: path.join(__dirname, '/'),
+    compress: true,
+    port: 8080,
+    proxy: {
+      '/api/**': 'http://localhost:3000',
+      '/assets/**': 'http://localhost:3000',
+    },
+  },
+};
